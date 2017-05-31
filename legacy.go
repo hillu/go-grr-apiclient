a5c57d4a6bc7ef2373fe1c1e36bf27899c4dd5bb
Hilko Bengen <bengen@hilluzination.de>
2017-05-31 15:41:13 +0200

    Add some legacy functins
    GRR as of 3.1.0.2 had not yet defined everything in terms of proper
    APIs with Protobuf-defined messages

---
+++ legacy.go
@@ -0,0 +1,63 @@
+package apiclient
+
+import (
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+)
+
+// The simple dialog box for "Force Cron", "Run Hunt", "Stop Hunt"
+// contain JS snippets similar to the following:
+//
+// grr.ExecuteRenderer(
+//     "ConfirmationDialogRenderer.Layout",
+//     {
+//         "check_access_subject": "aff4:/cron/CleanCronJobs",
+//         "unique": 1045,
+//         "cron_urn": "aff4:/cron/CleanCronJobs",
+//         "renderer": "ForceRunCronJobConfirmationDialog",
+//         "id": "legacy_6"
+//     });
+func (c *APIClient) executeConfirmationDialogRenderer(renderer string, params url.Values) (*http.Response, error) {
+	u := *c.BaseURL
+	u.Path = path.Join(u.Path, "/render/RenderAjax", renderer)
+	params.Set("renderer", renderer)
+	rq, err := http.NewRequest("POST", u.String(), strings.NewReader(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	rq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return c.DoRequest(rq)
+}
+
+// ForceRunCronLegacy schedules the cron hunt cronId to be run
+// immediately.
+//
+// This function uses a legacy (pre-API) interface.
+func (c *APIClient) ForceRunCronLegacy(cronId string) (*http.Response, error) {
+	return c.executeConfirmationDialogRenderer(
+		"ForceRunCronJobConfirmationDialog",
+		url.Values{"cron_urn": []string{cronId}},
+	)
+}
+
+// RunHuntLegacy activates hunt huntId.
+//
+// This function uses a legacy (pre-API) interface.
+func (c *APIClient) RunHuntLegacy(huntId string) (*http.Response, error) {
+	return c.executeConfirmationDialogRenderer(
+		"RunHuntConfirmationDialog",
+		url.Values{"hunt_id": []string{huntId}},
+	)
+}
+
+// StopHuntLegacy deactivates hunt huntId.
+//
+// This function uses a legacy (pre-API) interface.
+func (c *APIClient) StopHuntLegacy(huntId string) (*http.Response, error) {
+	return c.executeConfirmationDialogRenderer(
+		"StopHuntConfirmationDialog",
+		url.Values{"hunt_id": []string{huntId}},
+	)
+}
