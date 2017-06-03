33be1e06e1f782a299283cdc256ea198ad28f033
Hilko Bengen <bengen@hilluzination.de>
2017-06-03 15:41:53 +0200

    Explicitly set user, password in DoRequest
    This may not be necessary for "regular" functions, but through direct
    use of DoRequest with a http.Request object that is not based upon
    BaseURL -- and thus does not contain the credentials

--- apiclient.go
+++ apiclient.go
@@ -63,6 +63,8 @@ func (c *APIClient) getCSRFToken() string {
 // if the GRR server returns a JSON document.
 func (c *APIClient) DoRequest(rq *http.Request) (*http.Response, error) {
 	rq.Header.Set("x-csrftoken", c.getCSRFToken())
+	pass, _ := c.BaseURL.User.Password()
+	rq.SetBasicAuth(c.BaseURL.User.Username(), pass)
 	rs, err := c.client().Do(rq)
 	if err != nil {
 		return nil, err
